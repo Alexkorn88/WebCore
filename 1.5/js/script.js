40dd98b326889d29439b3a4956033e0178d0b2c6
Alexkorn88 <alex_korn_pochta@mail.ru>
2022-06-07 19:02:04 +0300

    create 1.5

---
+++ 1.5/js/script.js
@@ -0,0 +1,61 @@
+
+const slider = document.querySelector('.swiper');
+
+let mobSwiper;
+
+function mobileSlider() {
+    if (window.innerWidth <= 760 && slider.dataset.mobile == 'false') {
+        mobSwiper = new Swiper('.swiper', {
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+
+            spaceBetween: 0,
+            slidesPerView: 1.1
+        });
+
+        slider.dataset.mobile = 'true';
+    }
+
+    if (window.innerWidth > 760) {
+        slider.dataset.mobile = 'false';
+        
+        if (slider.classList.contains('swiper-initialized')) {
+            mobSwiper.destroy();
+        }
+        
+    }
+}
+
+mobileSlider();
+
+window.addEventListener('resize', () => {
+    mobileSlider();
+});
+
+let buttonOpen = document.querySelector('.expand__btn-open')
+let buttonClose = document.querySelector('.expand__btn-close')
+let mainOpen = document.querySelector('.main__open')
+
+buttonOpen.classList.remove('open')
+
+buttonOpen.addEventListener('click', function () {
+    buttonOpen.classList.remove('open')
+    if(document.querySelector('.main__open')) {
+        mainOpen.classList.remove('main__open')
+        buttonOpen.classList.add('open')
+        buttonClose.classList.remove('close')
+    } 
+    
+  });
+
+  buttonClose.addEventListener('click', function () {
+    buttonClose.classList.remove('close')
+    if(!document.querySelector('.main__open')) {
+        mainOpen.classList.add('main__open')
+        buttonClose.classList.add('close')
+        buttonOpen.classList.remove('open')
+    } 
+    
+  });
